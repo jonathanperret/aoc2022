6e624e47d01ab086f1cf2f6b0ed9632889d9c439
Jonathan Perret <j-github@jonathanperret.net>
2022-12-03 07:13:45 +0100

    day3 cleanup

--- tests/Day3.elm
+++ tests/Day3.elm
@@ -3,8 +3,10 @@ module Day3 exposing (..)
 import Day3Input exposing (input)
 import Expect
 import Fuzz
-import Test exposing (..)
+import List.Extra
 import Set
+import Test exposing (..)
+
 
 example1 =
     """vJrwpWtwJgWrhcsFMMfFFhFp
@@ -18,95 +20,101 @@ CrZsJsPPZsGzwwsLwLmpwMDw"""
 suite : Test
 suite =
     describe "day 3"
-        [
-            describe "part 1"
-            [
-                test "commonPriority" <| \_ -> "vJrwpWtwJgWrhcsFMMfFFhFp" |> commonPriority |>
-                    Expect.equal 16
-                ,test "example" <| \_ -> example1 |> part1 |> Expect.equal 157
-                ,test "input" <| \_ -> input |> part1 |> Expect.equal 8105
+        [ describe "part 1"
+            [ test "commonPriority" <|
+                \_ ->
+                    "vJrwpWtwJgWrhcsFMMfFFhFp"
+                        |> commonPriority
+                        |> Expect.equal 16
+            , test "example" <| \_ -> example1 |> part1 |> Expect.equal 157
+            , test "input" <| \_ -> input |> part1 |> Expect.equal 8105
             ]
-            ,describe "part 2"
-            [
-                 test "foundInSacks" <| \_ -> (foundInSacks (String.lines example1) 'r') |> Expect.equal 4
-                ,test "commonItemPrio" <| \_ -> (commonItemPrio (String.lines example1 |> List.take 3)) |> Expect.equal 18
-                ,test "example" <| \_ -> example1 |> part2 |> Expect.equal 70
-                ,test "input" <| \_ -> input |> part2 |> Expect.equal 2363
+        , describe "part 2"
+            [ test "foundInSacks" <| \_ -> foundInSacks (String.lines example1) 'r' |> Expect.equal 4
+            , test "commonItemPrio" <| \_ -> commonItemPrio (String.lines example1 |> List.take 3) |> Expect.equal 18
+            , test "example" <| \_ -> example1 |> part2 |> Expect.equal 70
+            , test "input" <| \_ -> input |> part2 |> Expect.equal 2363
             ]
         ]
 
-part2: String -> Int
+
+part2 : String -> Int
 part2 input =
     let
-        lines = String.lines input
+        lines =
+            String.lines input
 
-        (_, result) = next3Sacks (lines, 0)
+        groups =
+            List.Extra.groupsOf 3 lines
     in
-        result
+    groups |> List.map commonItemPrio |> List.sum
 
-next3Sacks: (List String, Int) -> (List String, Int)
-next3Sacks (lines, total) =
-    let
-        first3 = List.take 3 lines
-        rest = List.drop 3 lines
-    in
-        case first3 of
-            [] -> ([], total)
-            _ -> next3Sacks (rest, total + commonItemPrio first3)
 
-commonItemPrio: List String -> Int
+commonItemPrio : List String -> Int
 commonItemPrio sackStrs =
     let
-        items = sackStrs
-            |> String.concat
-            |> String.toList
-            |> Set.fromList
-            |> Set.toList
-        stats = items
-            |> List.map (\item -> (item, foundInSacks sackStrs item))
-        stats3 = stats
-            |> List.filter (\(item,n) -> n == 3)
-        prios = stats3
-            |> List.map (\(item,n) -> item)
-            |> List.map itemPriority
-    in List.sum prios
+        items =
+            sackStrs
+                |> String.concat
+                |> String.toList
+                |> List.Extra.unique
 
-itemPriority: Char -> Int
+        stats =
+            items
+                |> List.map (\item -> ( item, foundInSacks sackStrs item ))
+
+        stats3 =
+            stats
+                |> List.filter (\( item, n ) -> n == 3)
+
+        prios =
+            stats3
+                |> List.map (\( item, n ) -> item)
+                |> List.map itemPriority
+    in
+    List.sum prios
+
+
+itemPriority : Char -> Int
 itemPriority item =
     case Char.isLower item of
-        True -> (Char.toCode item) - (Char.toCode 'a') + 1
-        False -> (Char.toCode item) - (Char.toCode 'A') + 27
+        True ->
+            Char.toCode item - Char.toCode 'a' + 1
 
-foundInSacks: List String -> Char -> Int
+        False ->
+            Char.toCode item - Char.toCode 'A' + 27
+
+
+foundInSacks : List String -> Char -> Int
 foundInSacks sackStrs item =
-    let
-        sacks = sackStrs |> List.filter (String.any (\c -> c == item))
-    in List.length sacks
+    sackStrs
+        |> List.Extra.count (String.any (\c -> c == item))
 
-commonPriority: String -> Int
+
+commonPriority : String -> Int
 commonPriority sackstr =
     let
-        items = String.toList sackstr
-        left = List.take ((List.length items) // 2) items
-            --|> Debug.log "left"
-        right = List.drop ((List.length items) // 2) items
-            --|> Debug.log "right"
-        leftSet = Set.fromList left
-            --|>Debug.log "leftSet"
-        rightSet = Set.fromList right
-            --|>Debug.log "rightSet"
+        items =
+            String.toList sackstr
 
-        common = Set.intersect leftSet rightSet
-            --|> Debug.log "common"
+        ( left, right ) =
+            List.Extra.splitAt (List.length items // 2) items
 
-        commonItem = (Set.toList common |> List.head |> \i -> case i of
-                Just item -> item
-                Nothing -> Debug.todo "no common item")
-             --|> Debug.log "commonItem"
+        leftSet =
+            Set.fromList left
 
-        prio = itemPriority commonItem
+        rightSet =
+            Set.fromList right
+
+        common =
+            Set.intersect leftSet rightSet
+
+        commonItem =
+            Set.toList common
+                |> List.head
+                |> Maybe.withDefault '_'
     in
-        prio
+    itemPriority commonItem
 
 
 part1 : String -> Int
@@ -116,4 +124,4 @@ part1 input =
             String.lines input
     in
     List.map commonPriority lines
-    |> List.sum
+        |> List.sum
