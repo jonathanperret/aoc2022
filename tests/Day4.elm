e4ec0ed22d1757be92ce59c8a3a696168097117f
Jonathan Perret <j-github@jonathanperret.net>
2022-12-04 07:29:11 +0100

    day 4 cleanup

--- tests/Day4.elm
+++ tests/Day4.elm
@@ -6,7 +6,7 @@ import Fuzz
 import List.Extra
 import Set
 import Test exposing (..)
-
+import AocUtil exposing (..)
 
 example1 =
     """2-4,6-8
@@ -21,16 +21,15 @@ suite : Test
 suite =
     describe "day 4"
         [ describe "part 1"
-            [
-             test "example" <| \_ -> example1 |> part1 |> Expect.equal 2
+            [ test "example" <| \_ -> example1 |> part1 |> Expect.equal 2
             , test "input" <| \_ -> input |> part1 |> Expect.equal 496
             ]
         , describe "part 2"
-            [
-              test "example" <| \_ -> example1 |> part2 |> Expect.equal 4
+            [ test "example" <| \_ -> example1 |> part2 |> Expect.equal 4
             , test "input" <| \_ -> input |> part2 |> Expect.equal 847
             ]
         ]
+
 
 parse : String -> List (List (List Int))
 parse input =
@@ -38,38 +37,46 @@ parse input =
         lines =
             String.lines input
 
-        parse1 line = line
-            |> String.split ","
-            |> List.map (String.split "-" >> List.map (String.toInt >> Maybe.withDefault 0))
+        parse1 line =
+            line
+                |> String.split ","
+                |> List.map (String.split "-" >> List.map (String.toInt >> fromJust))
 
-        pairs = lines |> List.map parse1
-    in pairs
+        pairs =
+            lines |> List.map parse1
+    in
+    pairs
 
 
 part1 : String -> Int
 part1 input =
     let
-        check pair = case pair of
-            [[a,b],[c,d]] ->
-                (a >= c && b <= d)
-                || (c >= a && d <= b)
-            _ -> False
+        check pair =
+            case pair of
+                [ [ a, b ], [ c, d ] ] ->
+                    (a >= c && b <= d)
+                        || (c >= a && d <= b)
 
+                _ ->
+                    False
     in
-        parse input
+    parse input
         |> List.Extra.count check
+
 
 part2 : String -> Int
 part2 input =
     let
-        check pair = case pair of
-            [[a,b],[c,d]] ->
-                (b >= c && b <= d)
-                || (a >= c && a <= d)
-                || (c >= a && c <= b)
-                || (d >= a && d <= b)
-            _ -> False
-    in
-        parse input
-        |> List.Extra.count check
+        check pair =
+            case pair of
+                [ [ a, b ], [ c, d ] ] ->
+                    (b >= c && b <= d)
+                        || (a >= c && a <= d)
+                        || (c >= a && c <= b)
+                        || (d >= a && d <= b)
 
+                _ ->
+                    False
+    in
+    parse input
+        |> List.Extra.count check
