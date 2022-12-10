caf34e6896fd08b118c4dc30a4b790ddf8bf00f0
Jonathan Perret <j-github@jonathanperret.net>
2022-12-10 18:16:51 +0100

    extract day10 code from test file

---
+++ src/Day10.elm
@@ -0,0 +1,66 @@
+module Day10 exposing (..)
+
+import List as L
+import List.Extra as LE
+
+
+xvalues : String -> List Int
+xvalues input =
+    let
+        ( finalX, finalPastXs ) =
+            String.lines input
+                |> L.foldl
+                    (\instr ( x, pastXs ) ->
+                        case
+                            instr
+                                |> String.words
+                                |> List.tail
+                                |> Maybe.andThen List.head
+                                |> Maybe.andThen String.toInt
+                        of
+                            Just val ->
+                                ( x + val, x :: x :: pastXs )
+
+                            _ ->
+                                ( x, x :: pastXs )
+                    )
+                    ( 1, [] )
+    in
+    (finalX :: finalPastXs) |> L.reverse
+
+
+part1 input =
+    xvalues input
+        |> L.indexedMap
+            (\c x ->
+                if (c + 1 - 20 |> modBy 40) == 0 then
+                    Just ( c + 1, x )
+
+                else
+                    Nothing
+            )
+        |> L.filterMap identity
+        |> L.map (\( c, x ) -> c * x)
+        |> L.sum
+
+
+part2 : String -> List String
+part2 input =
+    xvalues input
+        |> L.indexedMap
+            (\c x ->
+                let
+                    col =
+                        modBy 40 c
+
+                    hit =
+                        x == col || (x - 1) == col || (x + 1) == col
+                in
+                if hit then
+                    "#"
+
+                else
+                    "."
+            )
+        |> LE.groupsOf 40
+        |> L.map (String.join "")
--- tests/Day10Tests.elm
+++ tests/Day10Tests.elm
@@ -1,18 +1,10 @@
 module Day10Tests exposing (..)
 
 import AocUtil exposing (..)
-import Array exposing (Array)
+import Day10 exposing (..)
 import Day10Input exposing (input)
-import Debug as D
 import Expect
-import Fuzz
-import List as L
-import List.Extra as LE
-import Set
-import String as S
-import String.Extra
 import Test exposing (..)
-import Tuple
 
 
 example1 =
@@ -205,65 +197,3 @@ suite =
                             ]
             ]
         ]
-
-
-xvalues : String -> List Int
-xvalues input =
-    let
-        ( finalX, finalPastXs ) =
-            String.lines input
-                |> L.foldl
-                    (\instr ( x, pastXs ) ->
-                        case
-                            instr
-                                |> String.words
-                                |> List.tail
-                                |> Maybe.andThen List.head
-                                |> Maybe.andThen String.toInt
-                        of
-                            Just val ->
-                                ( x + val, x :: x :: pastXs )
-
-                            _ ->
-                                ( x, x :: pastXs )
-                    )
-                    ( 1, [] )
-    in
-    (finalX :: finalPastXs) |> L.reverse
-
-
-part1 input =
-    xvalues input
-        |> L.indexedMap
-            (\c x ->
-                if (c + 1 - 20 |> modBy 40) == 0 then
-                    Just ( c + 1, x )
-
-                else
-                    Nothing
-            )
-        |> L.filterMap identity
-        |> L.map (\( c, x ) -> c * x)
-        |> L.sum
-
-
-part2 : String -> List String
-part2 input =
-    xvalues input
-        |> L.indexedMap
-            (\c x ->
-                let
-                    col =
-                        modBy 40 c
-
-                    hit =
-                        x == col || (x - 1) == col || (x + 1) == col
-                in
-                if hit then
-                    "#"
-
-                else
-                    "."
-            )
-        |> LE.groupsOf 40
-        |> L.map (String.join "")
