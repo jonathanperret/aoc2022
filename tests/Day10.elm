dcff7c2fd02983bf046f61d95b3b5b6cbc472c9f
Jonathan Perret <j-github@jonathanperret.net>
2022-12-10 09:10:21 +0100

    day10 cleanup

--- tests/Day10.elm
+++ tests/Day10.elm
@@ -1,19 +1,18 @@
 module Day10 exposing (..)
 
 import AocUtil exposing (..)
+import Array exposing (Array)
 import Day10Input exposing (input)
+import Debug as D
 import Expect
 import Fuzz
-import List.Extra
+import List as L
 import List.Extra as LE
 import Set
+import String as S
 import String.Extra
 import Test exposing (..)
 import Tuple
-import String as S
-import List as L
-import Debug as D
-import Array exposing (Array)
 
 
 example1 =
@@ -21,7 +20,9 @@ example1 =
 addx 3
 addx -5"""
 
-example2 = """addx 15
+
+example2 =
+    """addx 15
 addx -11
 addx 6
 addx -3
@@ -173,107 +174,96 @@ suite : Test
 suite =
     describe "day 10"
         [ describe "part 1"
-            [
-              test "noop" <| \_ ->{ startcycle=9, endcycle=10, x=42, prevx=41 } |> step "noop" |> Expect.equal { startcycle=10, endcycle=11, x=42, prevx=42 }
+            [ test "xvalues" <| \_ -> example1 |> xvalues |> Expect.equal [ 1, 1, 1, 4, 4, -1 ]
             , test "example2" <| \_ -> example2 |> part1 |> Expect.equal 13140
             , test "input" <| \_ -> input |> part1 |> Expect.equal 14720
             ]
         , describe "part 2"
-            [ test "example2" <| \_ -> example2 |> part2 |> Expect.equal
-                [ "##..##..##..##..##..##..##..##..##..##.."
-                , "###...###...###...###...###...###...###."
-                , "####....####....####....####....####...."
-                , "#####.....#####.....#####.....#####....."
-                , "######......######......######......####"
-                , "#######.......#######.......#######....." ]
-            , test "input" <| \_ -> input |> part2 |> Expect.equal 
-                [ "####.####.###..###..###..####.####.####."
-                , "#.......#.#..#.#..#.#..#.#.......#.#...."
-                , "###....#..###..#..#.###..###....#..###.."
-                , "#.....#...#..#.###..#..#.#.....#...#...."
-                , "#....#....#..#.#....#..#.#....#....#...."
-                , "#....####.###..#....###..#....####.#...."]
+            [ test "example2" <|
+                \_ ->
+                    example2
+                        |> part2
+                        |> Expect.equal
+                            [ "##..##..##..##..##..##..##..##..##..##.."
+                            , "###...###...###...###...###...###...###."
+                            , "####....####....####....####....####...."
+                            , "#####.....#####.....#####.....#####....."
+                            , "######......######......######......####"
+                            , "#######.......#######.......#######....."
+                            ]
+            , test "input" <|
+                \_ ->
+                    input
+                        |> part2
+                        |> Expect.equal
+                            [ "####.####.###..###..###..####.####.####."
+                            , "#.......#.#..#.#..#.#..#.#.......#.#...."
+                            , "###....#..###..#..#.###..###....#..###.."
+                            , "#.....#...#..#.###..#..#.#.....#...#...."
+                            , "#....#....#..#.#....#..#.#....#....#...."
+                            , "#....####.###..#....###..#....####.#...."
+                            ]
             ]
         ]
 
 
-type alias State = { startcycle: Int, endcycle: Int, x: Int, prevx: Int }
-
-step : String -> State -> State
-step instr state =
+xvalues : String -> List Int
+xvalues input =
     let
-        (duration, dx) = case String.split " " instr of
-            ["addx", argstr] -> (2, String.toInt argstr |> fromJust)
-            _ -> (1, 0)
+        ( finalX, finalPastXs ) =
+            String.lines input
+                |> L.foldl
+                    (\instr ( x, pastXs ) ->
+                        case
+                            instr
+                                |> String.words
+                                |> List.tail
+                                |> Maybe.andThen List.head
+                                |> Maybe.andThen String.toInt
+                        of
+                            Just val ->
+                                ( x + val, x :: x :: pastXs )
 
-    in { state | startcycle=state.endcycle, endcycle = state.endcycle+duration, x=state.x+dx, prevx=state.x }
+                            _ ->
+                                ( x, x :: pastXs )
+                    )
+                    ( 1, [] )
+    in
+    (finalX :: finalPastXs) |> L.reverse
 
 
 part1 input =
-    let
-        instrs =
-            input
-            |> S.lines
-        initial = { startcycle=0, endcycle=0, x=1, prevx=1 }
-        states = LE.scanl step initial instrs
-        --|> Debug.log "states"
+    xvalues input
+        |> L.indexedMap
+            (\c x ->
+                if (c + 1 - 20 |> modBy 40) == 0 then
+                    Just ( c + 1, x )
 
-        interestingStates =
-            states
-            |> L.filter
-                (\s ->
-                    let
-                        start = (s.startcycle + 20) // 40
-                        end = (s.endcycle + 20) // 40
-                        result = end > start
+                else
+                    Nothing
+            )
+        |> L.filterMap identity
+        |> L.map (\( c, x ) -> c * x)
+        |> L.sum
 
-                        _ = ((s.startcycle,s.endcycle),(start,end,result))
-                         --|>Debug.log "startend"
-                    in result)
-                    --|> Debug.log "states"
-        values =
-            interestingStates
-            |> L.map (\s -> s.prevx * (s.endcycle - modBy 2 s.endcycle))
-            |> Debug.log "values"
-    in
-    values |> L.sum
 
 part2 : String -> List String
 part2 input =
-    let
-        instrs =
-            input
-            |> S.lines
-        initial = { startcycle=0, endcycle=0, x=1, prevx=1 }
-        states = LE.scanl step initial instrs
-                        |> L.drop 1
-            --|> Debug.log "states"
-
-        xvalues =
-            L.range 0 239
-            |> L.map (\c ->
+    xvalues input
+        |> L.indexedMap
+            (\c x ->
                 let
-                    state =
-                        states
-                        |> LE.find (\s -> c >= s.startcycle && c < s.endcycle)
+                    col =
+                        modBy 40 c
+
+                    hit =
+                        x == col || (x - 1) == col || (x + 1) == col
                 in
-                (c, state |> Maybe.map .prevx)
-                )
-            --|> Debug.log "xvalues"
+                if hit then
+                    "#"
 
-        pixels =
-            xvalues
-            |> L.map (\(c,mx) ->
-                let
-                    col = modBy 40 c
-                    x = mx |> Maybe.withDefault -100
-                    hit = x == col || (x - 1) ==col || (x + 1) == col
-                    _ = ((c,col),(x,hit))
-
-                in if hit then "#" else "."
+                else
+                    "."
             )
-            |> LE.groupsOf 40
-            |> L.map (String.join "")
-            --|> Debug.log "pixels"
-    in
-    pixels
+        |> LE.groupsOf 40
+        |> L.map (String.join "")
