f08a7b34b209e81e948f015697011e728d59c4c4
Jonathan Perret <j-github@jonathanperret.net>
2022-12-17 09:18:16 +0100

    day 17 part 2

--- tests/Day17Tests.elm
+++ tests/Day17Tests.elm
@@ -15,7 +15,7 @@ import List as L
 import Debug as D
 import Array exposing (Array)
 import Regex
-
+import Integer
 
 rockShapes = [
       [(0,2),(0,3),(0,4),(0,5)]
@@ -30,28 +30,25 @@ example1 = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
 suite : Test
 suite =
     describe "day 17"
-        [ describe "part 1"
+        [ describe "part 2"
             [
               test "dropOne" <| \_ -> dropOne emptyField ">>><oooooo"
                 [(3,2),(3,3),(3,4),(3,5)]
                 |> Expect.equal
-                (Set.fromList [(0,2),(0,3),(0,4),(0,5)], "oooooo")
+                (Set.fromList [(0,2),(0,3),(0,4),(0,5)], "oooooo", 3)
             , test "dropOne2" <| \_ -> dropOne
                 (Set.fromList [(0,2),(0,3),(0,4),(0,5)])
                 "<><>oooo"
                 ([(4,3),(5,2),(5,3),(5,4),(6,3)])
                 |> Expect.equal
                 (Set.fromList [(0,2),(0,3),(0,4),(0,5),
-                    (1,3),(2,2),(2,3),(2,4),(3,3)], "oooo")
+                    (1,3),(2,2),(2,3),(2,4),(3,3)], "oooo", 3)
             , test "dropNew" <| \_ -> dropNew emptyField ">>><ooo"
                 0
-                |> Expect.equal (Set.fromList [(0,2),(0,3),(0,4),(0,5)], "ooo")
-            , test "example" <| \_ -> example1 |> part1 |> Expect.equal 3068
-            , test "input" <| \_ -> input |> part1 |> Expect.equal 3171
-            ]
-        , skip <| describe "part 2"
-            [ test "example" <| \_ -> example1 |> part2 |> Expect.equal 0
-            , skip <| test "input" <| \_ -> input |> part2 |> Expect.equal 0
+                |> Expect.equal (Set.fromList [(0,2),(0,3),(0,4),(0,5)], "ooo", 3)
+            , test "example short" <| \_ -> example1 |> part2 210 "2022" |> Expect.equal "3068"
+            , test "example" <| \_ -> example1 |> part2 100 "1000000000000" |> Expect.equal "1514285714288"
+            , test "input" <| \_ -> input |> part2 5000 "1000000000000" |> Expect.equal "1586627906921"
             ]
         ]
 
@@ -63,7 +60,7 @@ type alias Field = Set Point
 emptyField: Field
 emptyField = Set.empty
 
-dropOne: Field -> String -> Rock -> (Field, String)
+dropOne: Field -> String -> Rock -> (Field, String, Int)
 dropOne field jets rock =
     let
         hitsField f r =
@@ -119,8 +116,8 @@ dropOne field jets rock =
                 Nothing -> (pasteRock f r2, Nothing)
                 _ -> (f, r3)
 
-        dropFully : Field -> Rock -> String -> (Field, String)
-        dropFully f r js =
+        dropFully : Field -> Rock -> String -> Int -> (Field, String, Int)
+        dropFully f r js count =
             case S.uncons js of
                 Just (jet, js2) ->
                     let
@@ -128,11 +125,11 @@ dropOne field jets rock =
                         (f2, mr2) = step (f, r) jet
                     in
                     case mr2 of
-                        Nothing -> (f2, js2)
-                        Just r2 -> dropFully f2 r2 js2
+                        Nothing -> (f2, js2, count)
+                        Just r2 -> dropFully f2 r2 js2 (count+1)
                 Nothing -> D.todo "out of jets"
     in
-    dropFully field rock jets
+    dropFully field rock jets 0
     --|> Debug.log "dropFully"
 
 towerHeight: Field -> Int
@@ -143,7 +140,7 @@ towerHeight field =
         Nothing -> 0
         Just (i,j) -> i + 1
 
-dropNew: Field -> String -> Int -> (Field, String)
+dropNew: Field -> String -> Int -> (Field, String, Int)
 dropNew field jets rockIdx =
     let
         h = towerHeight field
@@ -152,22 +149,133 @@ dropNew field jets rockIdx =
     in
     dropOne field jets rock
 
-part1 input =
+cleanupField: Field -> Field
+cleanupField field =
     let
+        h = towerHeight field
+        rows =
+            field
+            |> Set.toList
+            |> LE.groupWhile (\(i1,j1) (i2,j2) -> i1==i2)
+            |> L.map (\(p,ps)->p::ps)
+            --|>D.log "rows"
+        --_ = rows |> L.map (L.length) |> D.log "rows"
+        fullRow =
+            rows
+            |> L.reverse
+            |> LE.find (\row -> L.length row == 7)
+    in
+    case fullRow of
+        Nothing -> field
+        Just ((fullI,_)::_) ->
+            field
+            --|> D.log "before"
+            |> Set.filter (\(i,j) -> i >= fullI)
+            --|> D.log "filtered"
+        Just [] -> Debug.todo "bad row"
+
+asInt i =
+    case Integer.toString i |> String.toInt of
+        Nothing -> D.todo "not int"
+        Just n -> if n < 10000000 then n
+            else D.todo "too big"
+
+
+part2 testIter iterStr input =
+    let
+        iter = Integer.fromString iterStr |> fromJust
         jets = String.repeat 10 input
         refill js =
             if S.length js < 1000 then
                 js ++ jets
             else
                 js
-        times = L.range 0 2021
-        (field, _) =
-            times
-            |> L.foldl (\t (f,js) -> dropNew f (refill js) t) (emptyField, jets)
-        _ = field
-            ---|> D.log "final"
-    in
-    towerHeight field
+        rockIndexes = L.range 0 (testIter-1)
+        (field, _, (counts,deltas)) =
+            rockIndexes
+            |> L.foldl (\t (f,js,(cnts,ds)) ->
+                let
+                    (f2,js2,cnt) = dropNew f (refill js) t
+                    delta = towerHeight f2 - towerHeight f
+                    f3 = cleanupField f2
+                in
+                (f3,js2,(cnts ++ [cnt],ds++[delta]))) (emptyField, jets, ([],[]))
+        --_ = field |> D.log "final"
+        --_ = counts |> D.log "counts"
+        --_ = deltas |> D.log "deltas"
 
-part2: String -> Int
-part2 input = 0
+        cycleLength =
+            case
+                L.range 10 testIter
+                |> L.reverse
+                |> LE.find (\length ->
+                    let
+                        groups =
+                            deltas
+                            |> L.reverse
+                            |> LE.groupsOf length
+                    in
+                        case groups of
+                            (last::previous::_) -> 
+                                (
+                                    last 
+                                    --|> D.log "last"
+                                )
+
+                                ==
+                                (
+                                    previous
+                                    --|> D.log "prev"
+                                )
+                            _ -> False
+                )
+            of
+                Nothing -> Debug.todo "no cycle found"
+                Just n -> n |> Debug.log "cycleLength"
+
+        cycle = deltas
+            |> L.reverse
+            |> L.take cycleLength
+            |> L.reverse
+            --|> D.log "cycle"
+
+        beforeCycle =
+            deltas
+            |> L.reverse
+            |> L.drop cycleLength
+            |> L.reverse
+            --|> D.log "before"
+
+        _ = L.length beforeCycle |> D.log "lengthBefore"
+
+        heightBefore = beforeCycle |> L.foldl (+) 0
+            |> D.log "heihtBefore"
+
+        cycleHeight = cycle |> L.foldl (+) 0
+            |> D.log "cycleHeight"
+
+        (cycleCount, lastCycleLength) = (Integer.divmod
+                (Integer.sub iter (Integer.fromInt (L.length beforeCycle)))
+                (Integer.fromInt cycleLength)
+            )
+            |> fromJust
+
+        _ = cycleCount |> Integer.toString |> D.log "cycleCount"
+        _ = lastCycleLength |> Integer.toString |> D.log "lastCycleLength"
+
+        lastCycle =
+            cycle
+            |> L.take (lastCycleLength |> asInt)
+            --|> D.log "lastCycle"
+
+        lastCycleHeight = lastCycle |> L.foldl (+) 0
+            |> D.log "lastCycleHeight"
+
+    in
+    (Integer.add
+        (Integer.add
+            (Integer.fromInt heightBefore)
+            (Integer.mul cycleCount (Integer.fromInt cycleHeight))
+        )
+        (Integer.fromInt lastCycleHeight))
+    |> Integer.toString
