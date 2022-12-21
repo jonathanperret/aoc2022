0612aab45291ddef287928ad8859e03c9b0428df
Jonathan Perret <j-github@jonathanperret.net>
2022-12-21 06:19:01 +0100

    day 21 part 1

--- tests/Day21Tests.elm
+++ tests/Day21Tests.elm
@@ -7,6 +7,7 @@ import Fuzz
 import List.Extra
 import List.Extra as LE
 import Set exposing (Set)
+import Dict exposing (Dict)
 import String.Extra
 import Test exposing (..)
 import Tuple
@@ -15,18 +16,32 @@ import List as L
 import Debug as D
 import Array exposing (Array)
 import Regex
+import String.Interpolate exposing (interpolate)
+import Integer exposing (Integer)
 
-
-example1 = """paste
-here"""
+example1 = """root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32"""
 
 
 suite : Test
 suite =
     describe "day 21"
         [ describe "part 1"
-            [ test "example" <| \_ -> example1 |> part1 |> Expect.equal 0
-            , skip <| test "input" <| \_ -> input |> part1 |> Expect.equal 0
+            [ test "example" <| \_ -> example1 |> part1 |> Integer.toString |> Expect.equal "152"
+            , test "input" <| \_ -> input |> part1 |> Integer.toString |> Expect.equal "0"
             ]
         , skip <| describe "part 2"
             [ test "example" <| \_ -> example1 |> part2 |> Expect.equal 0
@@ -34,14 +49,64 @@ suite =
             ]
         ]
 
+type alias Monkey = { name: String, op: Op }
+
+type Op =
+    Literal Integer
+    | Add String String
+    | Sub String String
+    | Mul String String
+    | Div String String
+
+parseLine: String -> Monkey
+parseLine line =
+    case Regex.find (Regex.fromString "[^ :]+" |> Maybe.withDefault Regex.never) line
+        |> List.map .match of
+        [ dest, litStr ] -> { name=dest, op= Literal (Integer.fromString litStr|>fromJust) }
+        [ dest, arg1, opStr, arg2 ] ->
+            { name=dest, op =
+                case opStr of
+                    "+" -> Add arg1 arg2
+                    "-" -> Sub arg1 arg2
+                    "*" -> Mul arg1 arg2
+                    "/" -> Div arg1 arg2
+                    _ -> D.todo ("bad op " ++ opStr)
+              }
+
+        _ -> D.todo ("bad line " ++ line)
+
+parse: String -> Dict String Monkey
+parse input =
+    input
+    |> S.lines
+    |> L.map parseLine
+    |> L.map (\m -> (m.name, m))
+    |> Dict.fromList
+
+
+eval: Dict String Monkey -> String -> Integer
+eval monkeys name =
+    let
+        targetMonkey = case Dict.get name monkeys of
+            Just m -> m
+            _ -> D.todo ("unknown monkey" ++ name)
+    in
+    case targetMonkey.op of
+        Literal n -> n
+        Add arg1 arg2 -> Integer.add (eval monkeys arg1) (eval monkeys arg2)
+        Sub arg1 arg2 -> Integer.sub (eval monkeys arg1) (eval monkeys arg2)
+        Mul arg1 arg2 -> Integer.mul (eval monkeys arg1) (eval monkeys arg2)
+        Div arg1 arg2 -> case Integer.div (eval monkeys arg1) (eval monkeys arg2) of
+            Just x -> x
+            _ -> D.todo ("division by zero at " ++ name)
+
 
 part1 input =
     let
-        data =
+        monkeys =
             input
-            |> S.lines
+            |> parse
     in
-    0
+    eval monkeys "root"
 
-part2: String -> Int
 part2 input = 0
